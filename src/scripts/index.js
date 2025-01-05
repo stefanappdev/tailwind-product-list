4b943f18032143d0bc4b9abd3e63761ede774540
stefanappdev <stevendking@protonmail.com>
2025-01-04 21:51:22 -0500

    added total price to trolley

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -34,8 +34,9 @@ let cartmenu=document.querySelector("#cart-menu");
 let cart=document.querySelector("#cart");
 let emptycart_img=document.querySelector("#cart-empty-img");	
 let emptycart_text=document.querySelector("#cart-empty-text");
-let cartcounter=document.querySelector("#cart-counter");				
-
+let cartcounter=document.querySelector("#cart-counter");		
+let cartcheckout=document.querySelector("#cart-checkout");		
+let carttotal=document.querySelector("#cart-total");	
 
 
 //btns to increase qty of items to trolley	
@@ -56,7 +57,8 @@ for(let x=0;x<counterqtys.length;x++){
 		counters[x].style.transform="translateY(-20px)";
 		counters[x].style.alignItems="center";
 		counters[x].style.justifyContent="center";
-		console.log("totalitems:",CART.getTotalItems())
+		console.log("total:",CART.getTotalPrice())
+		cartcounter.textContent=`Your Cart (${CART.getTotalItems()})`;
 		})
 	
 	
@@ -74,14 +76,19 @@ for(let x=0;x<counterqtys.length;x++){
 			//#update cart Array and cart item in trolley
 			if (item){
 				item.qty=parseInt(counterqtys[item.id].textContent);
-				console.log(CART.showContents());
+				//console.log(CART.showContents());
 				
 				let item_id=`#cart-item-${item.id}`;
 				let cartItem=document.querySelector(item_id);
 				let item_summary=document.querySelector(`#summary-item-${item.id}`)
 				item_summary.textContent=`${item.qty}x @${item.price} ${(item.price*item.qty).toString()}`;
-				CART.increaseTotalItems()
-				console.log("totalitems:",CART.getTotalItems())
+				CART.increaseTotalItems();
+				CART.increaseCartTotalcost(item.price)
+				cartcounter.textContent=`Your Cart (${CART.getTotalItems()})`;
+				carttotal.textContent=CART.getTotalPrice().toString();
+				console.log("total:",CART.getTotalPrice())
+				
+				
 			}
 			
 	});
@@ -109,8 +116,12 @@ for(let x=0;x<counterqtys.length;x++){
 				let item_summary=document.querySelector(`#summary-item-${item.id}`)
 				item_summary.textContent=`${item.qty}x @${item.price} ${(item.qty>1?currentprice-item.price:item.price).toString()}`;
 				CART.decreaseTotalItems();
-				console.log("totalitems:",CART.getTotalItems())
+				CART.decreaseCartTotalcost(item.price)
+				cartcounter.textContent=`Your Cart (${CART.getTotalItems()})`;
+				carttotal.textContent=CART.getTotalPrice().toString();
+				console.log("total:",CART.getTotalPrice())
 			}
+			
 	})
 	
 	
@@ -126,9 +137,11 @@ for(let x=0;x<counterqtys.length;x++){
 class shoppingCart{
 	
 	
-	constructor(cart,isActive=false,totalitems=0){
+	constructor(cart=[],isActive=false,totalitems=0,totalprice=0){
 		this.cart=cart;
 		this.totalitems=totalitems;
+		this.totalprice=totalprice;
+		this.isActive=isActive;
 	}
 	
 	
@@ -150,14 +163,18 @@ class shoppingCart{
 		if(this.findItem(id)){
 			updatedcart=this.cart.filter(item=>item.id!=id);
 			this.decreaseTotalItems(this.findItem(id).qty)
+			this.decreaseCartTotalcost(this.findItem(id).qty*this.findItem(id).price)
 			this.removeCartMenuItem(id);
 			this.cart=[...updatedcart];
-			console.log(this.cart)
-			
-				
+			cartcounter.textContent=`Your Cart (${this.getTotalItems()})`;
+			carttotal.textContent=this.getTotalPrice().toString();
+			//console.log(this.cart)
 		}
 		
-		
+		//check if cart empty
+		if(this.isEmpty()){
+				this.setisActive(false);
+			}
 	}
 	
 	
@@ -185,20 +202,28 @@ class shoppingCart{
 		let cartmenu_div=document.createElement("div");
 		let itemName=document.createElement("span");
 		let summary=document.createElement("span");
-		let deletebtn=document.createElement("a");
+		let deletebtn=document.createElement("img");
+		let circle=document.createElement("div");
 		let itemTotalPrice=item.qty*item.price;
 		console.log(itemTotalPrice);
 		itemName.textContent=item.name;
+		itemName.classList.add("itemname");
 		cartmenu_div.classList.add("cart-menu-item");
 		cartmenu_div.id=`cart-item-${item.id}`;
-		deletebtn.textContent="delete";
+		circle.classList.add("circle");
+		deletebtn.classList.add("deletebtn");
+		deletebtn.src="../public/images/icon-remove-item.svg";
+		
 		deletebtn.addEventListener("click",()=>this.deleteItem(item.id));
 		summary.id=`summary-item-${item.id}`;
 		summary.textContent=`${item.qty}x @${item.price} ${itemTotalPrice.toString()}`;
+		
 		cartmenu_div.appendChild(itemName);
-		cartmenu_div.appendChild(deletebtn);
+		circle.appendChild(deletebtn);
 		cartmenu_div.appendChild(summary);
 		cartmenu.appendChild(cartmenu_div);
+		
+		cartmenu_div.appendChild(circle);
 		
 	}
 	
@@ -226,19 +251,21 @@ class shoppingCart{
 			if(this.isEmpty()){
 				this.cart=[...this.cart,newItem];
 				this.setisActive(true);
-			    console.log(this.cart)
+			    //console.log(this.cart)
 				this.addCartMenuItem(newItem);
 				this.increaseTotalItems()
-				
-				
+				this.increaseCartTotalcost(newItem.price)
+				cartcounter.textContent=`Your Cart (${this.getTotalItems()})`;
+				carttotal.textContent=this.getTotalPrice().toString();
 			}else{
 				if(!this.findItem(id)){
 					this.cart=[...this.cart,newItem];
-					console.log(this.cart)
+					//console.log(this.cart)
 					this.addCartMenuItem(newItem);
 					this.increaseTotalItems()
-					
-					
+					this.increaseCartTotalcost(newItem.price)
+					cartcounter.textContent=`Your Cart (${this.getTotalItems()})`;
+					carttotal.textContent=this.getTotalPrice().toString();
 				}
 				
 			}
@@ -251,7 +278,11 @@ class shoppingCart{
 			this.isActive=true;
 			emptycart_img.style.display="none";
 			emptycart_text.style.display="none";
-			}else if(this.isEmpty){
+			cartcheckout.classList.remove("hidden");
+			
+			
+			
+			}else if(state===false){
 				this.isActive=false;
 				emptycart_img.style.display="block";
 				emptycart_text.style.display="block";
@@ -262,24 +293,42 @@ class shoppingCart{
 		showContents(){
 			return(this.cart);
 		}
+		
+		increaseCartTotalcost(price){
+			this.totalprice+=price;
+		}
+		
+		decreaseCartTotalcost(price){
+			this.totalprice-=price;
+		}
 	
+	
+	//increase amount of trolley items by 1
 		increaseTotalItems(amt=1){
 			this.totalitems+=amt;
 		}
 	
+	//decrease amount of trolley items by 1
 		decreaseTotalItems(amt=1){
 			this.totalitems-=amt;
 		}
 		
+		
+	//returns total number of items in trolley
 		getTotalItems(){
 			return this.totalitems;
+		}
+		
+		//returns total price of items in trolley
+		getTotalPrice(){
+			return this.totalprice;
 		}
 
 }
 
 //creates a new instance of a shoppingCart
 
-const CART=new shoppingCart([]);
+const CART=new shoppingCart();
 
 
 
